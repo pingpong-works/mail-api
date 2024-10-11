d318021877bf14e45be5f2006ea43a76cf9e8dcb
Lifesheep <scientist5@naver.com>
2024-10-11 17:57:13 +0900

    fix:#18 페이지네이션

--- src/main/java/com/mail/mail/repository/MailRepository.java
+++ src/main/java/com/mail/mail/repository/MailRepository.java
@@ -1,13 +1,12 @@
 package com.mail.mail.repository;
 
 import com.mail.mail.entity.Mail;
-import com.mail.mail.entity.MailAttach;
-import com.mail.mail.entity.MailImportant;
-import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 public interface MailRepository extends JpaRepository<Mail, Long>{
+    Page<Mail> findAllByOrderBySentAtDesc(Pageable pageable);
 }
